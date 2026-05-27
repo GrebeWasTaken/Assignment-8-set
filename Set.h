07f5b0f53d1859196a4f122164e59b8901fe09bb
A Student <student@nowhere.edu>
2026-05-27 05:44:20 +0000

    May 26, 2026, 10:44 PM

--- Set.h
+++ Set.h
@@ -19,7 +19,7 @@ class Set{
 
       T* newData = new T[m_capacity];
 
-      for (int i - 0; i < m_size; i++) {
+      for (int i = 0; i < m_size; i++) {
         newData[i] = m_data[i];
 
         delete[] m_data;
