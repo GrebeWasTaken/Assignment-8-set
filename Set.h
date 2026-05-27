9f22efcffd63135342dff1bb00950802e1ee3c45
A Student <student@nowhere.edu>
2026-05-27 05:32:33 +0000

    May 26, 2026, 10:32 PM

--- Set.h
+++ Set.h
@@ -28,7 +28,7 @@ class Set{
     bool contains(T item) const {
       for (int i = 0; i < m_size; i++) {
         if (m_data[i] == item){
-          r
+          return true;
         }
       }
     }
