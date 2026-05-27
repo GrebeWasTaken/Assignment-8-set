e8abd683f5ee474258bfdfd2501c00a95afabe84
A Student <student@nowhere.edu>
2026-05-27 06:20:40 +0000

    May 26, 2026, 11:20 PM

--- Set.h
+++ Set.h
@@ -60,9 +60,11 @@ public:
       m_data = new T[m_capacity];
 
       for (int i = 0; i < m_size; i++) {
-       m_data[i] = other.m_data[i];
+        m_data[i] = other.m_data[i];
       }
     }
+
+    return 
   }
 
   bool contains(T item) const
