9812b40f70aeb554bd38339aefe05009c4b646f4
A Student <student@nowhere.edu>
2026-05-27 06:19:40 +0000

    May 26, 2026, 11:19 PM

--- Set.h
+++ Set.h
@@ -56,8 +56,8 @@ public:
       delete[] m_data;
 
       m_capacity = other.m_capacity;
-    m_size = other.m_size;
-    m_data = new T[m_capacity];
+      m_size = other.m_size;
+      m_data = new T[m_capacity];
     }
   }
 
